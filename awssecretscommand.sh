60fe3acda3313bcdc63d69830271576fc3710da4
ashpad <41486598+ashpad@users.noreply.github.com>
2018-07-20 13:54:00 -0400

    Update awssecretscommand.sh

--- awssecretscommand.sh
+++ awssecretscommand.sh
@@ -1,7 +1,6 @@
- aws ec2 describe-instances \
-  --filter 'Name=tag:Name,Values=api' 'Name=instance-state-name,Values=running' | \
-  jq -r '.Reservations[].Instances[] | [.InstanceId, .PrivateIpAddress, .Tags[].Value] | @csv'
+# Get secrets
+aws secretsmanager get-secret-value --secret-id JoinADPasscode --region us-east-1
   
-  
+# Get the password using jQ  
   aws secretsmanager get-secret-value --secret-id JoinADPasscode --region us-east-1\
   |jq -r '.SecretString'|jq -r '.ADPasscode'
